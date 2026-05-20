54d030ab3bd3936a582b520eb35a53b63e2ea06e
Mohamed Boudra <boudra.moha@gmail.com>
2026-05-20 13:53:34 +0700

    Update startup redirect test

--- packages/app/src/app/index.tsx
+++ packages/app/src/app/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Redirect, usePathname } from "expo-router";
 import { StartupSplashScreen } from "@/screens/startup-splash-screen";
 import { useEarliestOnlineHostServerId, useHostRuntimeBootstrapState } from "@/app/_layout";
