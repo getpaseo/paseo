54ef859190209edb11e7488f3575c7c57dfb4d3a
Mohamed Boudra <boudra.moha@gmail.com>
2026-09-26 19:08:56 +0200

    refactor(server): remove Oh My Pi handling from the Pi provider

--- packages/server/src/server/agent/providers/pi/extensions/registry.ts
+++ packages/server/src/server/agent/providers/pi/extensions/registry.ts
@@ -1,6 +1,5 @@
 import { piAskUser } from "./pi-ask-user/index.js";
 import { piSubagents } from "./pi-subagents/index.js";
-import { ohMyPi } from "./oh-my-pi/index.js";
 import { piMcpAdapter } from "./pi-mcp-adapter/index.js";
 import { rpivTodo } from "./rpiv-todo/index.js";
 import { piExampleTodo } from "./pi-example-todo/index.js";
@@ -14,7 +13,6 @@ export const piExtensions = [
   piSubagents, // Delegation calls
   tintinwebPiSubagents,
   gotgenesPiSubagents,
-  ohMyPi, // Compatible runtime tools
   piMcpAdapter, // MCP proxy names
   rpivTodo,
   piExampleTodo,
